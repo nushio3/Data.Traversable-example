393a2eda9aefd3da3da75035f4013d388706b1fc
Takayuki Muranushi <muranushi@gmail.com>
2011-05-04 11:45:04 +0900

    more sample

--- Main.hs
+++ Main.hs
@@ -1,19 +1,24 @@
 {-# OPTIONS -Wall #-}
 import Data.Foldable
 import Data.Functor
+import Data.Monoid
 import Data.Traversable
 import Prelude hiding (mapM,fmap,foldl1,foldr1)
 import Tree
 
+age :: String -> Int
+age s = if s=="sazae" then 24 else if s=="katsuo" then 11 else 9
+
 main :: IO ()
 main = do
   let t = Node "sazae" (Node "katsuo" Empty Empty) (Node "wakame" Empty Empty)
-  Print t
+  print t
   _ <- mapM (putStrLn) t
-  Print $ mapM (const ["M", "F"]) t
+  print $ mapM (const ["M", "F"]) t
   print $ fmap (length) t
   print $ foldMap (:[]) t
   print $ foldMap (\x -> [x, x++"'s cat"]) t
+  print $ foldMap (Product . age) t
   putStrLn $ foldl1 (\x y -> " <"++x++" "++y++"> ") t
   putStrLn $ foldr1 (\x y -> " <"++x++" "++y++"> ") t
   return ()
